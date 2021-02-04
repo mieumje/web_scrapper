ce357e1d536975953e1b53c50159933eab4c5112
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-04 20:36:07 +0900

    #2 Beautiful Soup으로 추출하기
    indeed_soup변수 생성 후 출력

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ from bs4 import BeautifulSoup
 indeed_result = requests.get(
     'https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&limit=50')
 
+indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
 
-print(indeed_result.text)
+print(indeed_soup)
