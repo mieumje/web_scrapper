3614797207047344c4d4fed4b1e77838fb2f8340
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-09 21:07:37 +0900

    #4 각 페이지 요청하기
    #3~ #4.1 의 내용을 extract_indeed_pages()라는 함수로 선언

---
+++ indeed.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+INDEED_URL = "https://kr.indeed.com/jobs?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&limit=50&radius=25"
+
+
+def extract_indeed_pages():
+    indeed_result = requests.get(INDEED_URL)
+
+    indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+
+    pagination = indeed_soup.find("div", {"class": "pagination"})
+
+    # pagination 변수에 찾은 결과를 넣어줌
+    # 그 결과를 list로 만들어 pages 변수에 넣어줌
+
+    # pages를 가져온 다음
+    # 빈 array를 만들고
+    # pages에 있는 각 page마다 span을 찾아서
+    # 빈 array에 넣어준다.
+    links = pagination.find_all('a')
+    pages = []
+
+    for link in links[:-1]:  # '다음'버튼은 무시
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+    # 페이지 숫자 중 가장 큰 수, 배열의 마지막 요소
+    print(max_page)
