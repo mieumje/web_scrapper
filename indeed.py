08b52727b0608dcdcfe48465d9f91b107f714344
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-09 21:21:50 +0900

    #4 각 페이지 요청하기
    extract_indeed_jobs라는 새로운 함수 생성
    for 문으로 페이지 요청을 위한 작업 생성
    URL 수정, LIMIT 생성

--- indeed.py
+++ indeed.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://kr.indeed.com/jobs?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&limit=50&radius=25"
+LIMIT = 50
+URL = f"https://kr.indeed.com/jobs?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&limit={LIMIT}"
 
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
 
     soup = BeautifulSoup(result.text, 'html.parser')
 
@@ -27,3 +28,8 @@ def extract_indeed_pages():
     max_page = pages[-1]
     # 페이지 숫자 중 가장 큰 수, 배열의 마지막 요소
     return max_page
+
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        print(f"&start={page*LIMIT}")
