89e5162f178ab5e8f94e3e063e00ba30327c5cbc
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-09 21:10:26 +0900

    #4 각 페이지 요청하기
    indeed.py에서 생성한 함수 extract_indeed_pages를 import하고
    max_indeed_pages라는 변수에 extract_indeed_pages 함수 호출 후 출력

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages
 
-indeed_result = requests.get(
-    'https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=%ED%8C%8C%EC%9D%B4%EC%8D%AC&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch')
+max_indeed_pages = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-
-pagination = indeed_soup.find("div", {"class": "pagination"})
-
-# pagination 변수에 찾은 결과를 넣어줌
-# 그 결과를 list로 만들어 pages 변수에 넣어줌
-
-
-# pages를 가져온 다음
-# 빈 array를 만들고
-# pages에 있는 각 page마다 span을 찾아서
-# 빈 array에 넣어준다.
-links = pagination.find_all('a')
-pages = []
-
-for link in links[:-1]:  # '다음'버튼은 무시
-    pages.append(int(link.string))
-
-
-max_page = pages[-1]
-# 페이지 숫자 중 가장 큰 수, 배열의 마지막 요소
-
-for n in range(max_page):
-    print(f"start={n*50}")
+print(max_indeed_pages)
