f2b95e304298a292b2eab620e22476170b01c8df
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-16 21:09:08 +0900

    #7
    페이지가 없어져 limit 20으로 수정
    extract_ineed_jobs()함수에서
    두번째 for문에서
    title과 company를 추출하는 것을
    extract_jobs()함수로 선언
    
    extract_jobs()함수는 인자로 html을 받고
    line 51에서 인자로 들어간 result가 soup으로 추출한 html 정보

--- indeed.py
+++ indeed.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-LIMIT = 50
+LIMIT = 20
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&limit={LIMIT}"
 
 
@@ -30,19 +30,24 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_jobs(html):  # 인자로 soup을 준다.
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("div", {"class": "sjcl"}).find("span")
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = company_anchor.string
+    else:
+        company = company.string
+    return {'title': title, 'company': company}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
     # for page in range(last_page):
-    result = requests.get(f"{URL}&start={1*LIMIT}")
+    result = requests.get(f"{URL}&start={0*LIMIT}")
     soup = BeautifulSoup(result.text, 'html.parser')
     results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
     for result in results:
-        title = result.find("h2", {"class": "title"}).find("a")["title"]
-        company = result.find("div", {"class": "sjcl"}).find("span")
-        company_anchor = company.find("a")
-        if company_anchor is not None:
-            company = company_anchor.string
-        else:
-            company = company.string
-        print(title, company)
+        job = extract_jobs(result)
+        print(job)
     return jobs
