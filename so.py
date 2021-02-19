e88fef7831324bf0f6d248ec1fc1710b62224cd8
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-19 20:09:14 +0900

    #8 StackOverFlow에서 추출하기
    get_last_page()함수에서
    마지막 페이지를 return하게 하고
    get_jobs()함수에서
    last_page가 제대로 전달 되었는지 출력,
    결과는 #8-8과 같음

--- so.py
+++ so.py
@@ -9,10 +9,11 @@ def get_last_page():
     resulst = requests.get(URL)
     soup = BeautifulSoup(resulst.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    last_pages = pages[-2].get_text()
-    print(last_pages)
+    last_pages = pages[-2].get_text(strip=True)
+    return last_pages
 
 
 def get_jobs():
     last_page = get_last_page()
+    print(last_page)
     return []
