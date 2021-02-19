c77102157d84003f2625b0e18c650fd0df917d01
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-19 20:16:25 +0900

    #8 StackOverFlow에서 추출하기
    전체 page를 출력하기 위한 for문
    page가 0~마지막 페이지-1까지 출력되어
    1~마지막 페이지까지 출력되게
    +1해서 출력

--- so.py
+++ so.py
@@ -10,10 +10,16 @@ def get_last_page():
     soup = BeautifulSoup(resulst.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
     last_pages = pages[-2].get_text(strip=True)
-    return last_pages
+    return int(last_pages)  # for 문의 in range에 사용하기 위해 integer로 형변환
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(page + 1)
 
 
 def get_jobs():
     last_page = get_last_page()
-    print(last_page)
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
