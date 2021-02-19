b7e4aa78e60e95601aa0612f187e5e6e23929e48
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-19 19:48:45 +0900

    #8 StackOverFlow에서 추출하기
    so.py
    indeed.py와 같은 구조로 함수 생성
    get_last_page()함수에서
    result로 페이지를 요청하고
    soup을 통해 요청한 페이지의 html 정보를 가져온다
    
    main.py
    페이지 요청을 확인하기 위해
    get_jobs를 so.py에서 import하고
    get_so_jobs()를 호출

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from indeed import get_jobs as get_ineed_jobs
+from so import get_jobs as get_so_jobs
 
-indeed_jobs = get_ineed_jobs()
-
-print(indeed_jobs)
+#indeed_jobs = get_ineed_jobs()
+so_jobs = get_so_jobs()
--- so.py
+++ so.py
@@ -3,3 +3,14 @@ from bs4 import BeautifulSoup
 
 
 URL = f"https://stackoverflow.com/jobs?q=python&pg=2"
+
+
+def get_last_page():
+    resulst = requests.get(URL)
+    soup = BeautifulSoup(resulst.text, "html.parser")
+    print(soup)
+
+
+def get_jobs():
+    last_page = get_last_page()
+    return []
