37723fb065e8be5fce9bf70ae83d06026e7acc91
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-19 19:44:25 +0900

    #8 indeed 수정
    마지막 수정

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ LIMIT = 20
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=%ED%8C%8C%EC%9D%B4%EC%8D%AC&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
 
     soup = BeautifulSoup(result.text, 'html.parser')
@@ -30,7 +30,7 @@ def extract_indeed_pages():
     return max_page
 
 
-def extract_jobs(html):  # 인자로 soup을 준다.
+def extract_job(html):  # 인자로 soup을 준다.
     title = html.find("h2", {"class": "title"}).find("a")["title"]
     company = html.find("div", {"class": "sjcl"}).find("span")
     company_anchor = company.find("a")
@@ -41,13 +41,13 @@ def extract_jobs(html):  # 인자로 soup을 준다.
             company = company.string
         company = company.strip()
     else:
-        comapny = None
+        company = None
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
     job_id = html["data-jk"]
     return {'title': title, 'company': company, 'location': location, 'link': f"https://kr.indeed.com/%EC%B1%84%EC%9A%A9%EB%B3%B4%EA%B8%B0?jk={job_id}"}
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -55,12 +55,12 @@ def extract_indeed_jobs(last_page):
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
-            job = extract_jobs(result)
+            job = extract_job(result)
             jobs.append(job)
     return jobs
 
 
 def get_jobs():
-    last_indeed_page = extract_indeed_pages()
-    jobs = extract_indeed_jobs(last_indeed_page)
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from indeed import get_jobs as iget_jobs
+from indeed import get_jobs as get_ineed_jobs
 
-indeed_jobs = iget_jobs()
+indeed_jobs = get_ineed_jobs()
 
 print(indeed_jobs)
