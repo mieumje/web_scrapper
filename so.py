147045ec10e3a5090e3c3a6ad2989eaf3c372685
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-19 19:54:49 +0900

    #8 StackOverFlow에서 추출하기
    찾아낸 pagination에서 anchor 출력하기

--- so.py
+++ so.py
@@ -8,8 +8,8 @@ URL = f"https://stackoverflow.com/jobs?q=python&pg=2"
 def get_last_page():
     resulst = requests.get(URL)
     soup = BeautifulSoup(resulst.text, "html.parser")
-    pagination = soup.find("div", {"class": "s-pagination"})
-    print(pagination)
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    print(pages)
 
 
 def get_jobs():
