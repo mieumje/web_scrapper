6de6b9445d585b02501ab2a2a01de69b53edf415
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-22 17:20:33 +0900

    #9 StackOverFlow job 추출하기
    job이 출력되니 return문으로 결과 반환

--- so.py
+++ so.py
@@ -15,7 +15,7 @@ def get_last_page():
 
 def extract_job(html):
     title = html.find("h2", {"class": "mb4"}).find("a")["title"]
-    print(title)
+    return {'title': title}
 
 
 def extract_jobs(last_page):
