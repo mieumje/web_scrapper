de500cb03d161261a07ae46d7bdfb3fb947c3ce1
mieumje <66072832+mieumje@users.noreply.github.com>
2021-02-22 17:32:40 +0900

    #9 StackOverFlow company 추출하기
    span안의 span을 검사하지 않도록
    recursive함수를 False로 설정

--- so.py
+++ so.py
@@ -15,7 +15,8 @@ def get_last_page():
 
 def extract_job(html):
     title = html.find("h2", {"class": "mb4"}).find("a")["title"]
-    company = html.find("h3", {"class": "mb4"}).find_all("span")
+    company = html.find("h3", {"class": "mb4"}).find_all(
+        "span", recursive=False)
     print(company)
     return {'title': title}
 
