d0957b7d3c40c710e6956410d91ad0a35f689de3
mieumje <66072832+mieumje@users.noreply.github.com>
2021-04-06 22:30:59 +0900

    csv파일 만들기
    UnicodeEncodeError: 'cp949' codec can't encode character '\u4f1a' in position 17: illegal multibyte sequence
    이런 오류 나시면 save.py에서
    file = open("jobs.csv", mode = "w", encoding = "utf-8")

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from indeed import get_jobs as get_ineed_jobs
 from so import get_jobs as get_so_jobs
+from save import save_to_file
 
 indeed_jobs = get_ineed_jobs()
 so_jobs = get_so_jobs()
 
 jobs = so_jobs + indeed_jobs
-
-print(jobs)
+save_to_file(jobs)
